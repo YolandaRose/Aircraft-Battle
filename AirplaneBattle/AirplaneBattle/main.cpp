9f0daeaf92473604d75d9afdbaa23da4f3fc8bd3
Yolanda <liuyolanda@163.com>
2025-04-15 22:21:35 +0800

    1

--- AirplaneBattle/AirplaneBattle/main.cpp
+++ AirplaneBattle/AirplaneBattle/main.cpp
@@ -4,16 +4,16 @@
 int main() {
 
 	//1.创建图形界面窗口
-	initgraph(800, 800);//宽×高
+	initgraph(480, 700);//px像素：宽×高（按照图片大小）
 
 	//2.贴图
 	//定义变量
 	IMAGE img;
-	loadimage(&img,);//加载图片
+	
 	//赋值
-
+	loadimage(&img, "images/bk.png");//加载图片：容器，路径
 	//输出
-
+	putimage(0, 0, &img);//输出图片：坐标（从左上角0，0开始），容器
 
 	system("pause");
 	return 0;
