9e750715d599060e3ea1f91bc0688f5ffbb407da
Yolanda <liuyolanda@163.com>
2025-04-15 22:08:03 +0800

    initial

---
+++ AirplaneBattle/AirplaneBattle/main.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<easyx.h>
+
+int main() {
+
+	//1.创建图形界面窗口
+	initgraph(800, 800);//宽×高
+
+	//2.贴图
+	//定义变量
+	IMAGE img;
+	loadimage(&img,);//加载图片
+	//赋值
+
+	//输出
+
+
+	system("pause");
+	return 0;
+}
